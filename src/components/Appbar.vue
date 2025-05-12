64674122d5116ded19d33ab6bd7c401cf6e6539f
Brian Wolf <wolfbj32@gmail.com>
2025-05-11 21:57:58 -0400

    app bar centered

--- src/components/Appbar.vue
+++ src/components/Appbar.vue
@@ -1,47 +1,38 @@
 <template>
-  <!-- Top Toolbar -->
   <v-app-bar color="white" flat app height="80">
-    <v-container class="d-flex justify-space-between align-center">
-      <!-- Logo -->
-      <v-row align="center" no-gutters>
-          <v-img
-            src="@/assets/logo-depauw.png"
-            alt="Logo"
-            max-height="100"
-            contain
-            class="mr-4"
-          ></v-img>
-      </v-row>
-
-      <!-- Nav Links -->
-      <v-row class="d-none d-md-flex" align="center" no-gutters>
-        <v-btn
-          variant="text"
-          class="text-uppercase font-weight-bold"
-          color="primary"
-          to="/"
-          >Home</v-btn
-        >
-        <v-btn
+    <v-container class="d-flex align-center justify-center">
+      <img
+        src="@/assets/logo-depauw.png"
+        alt="Logo"
+        style="width: 100px; height: 100px; position: absolute; left: 20px"
+        contain
+      />
+      <v-btn
+        variant="text"
+        class="text-uppercase font-weight-bold"
+        color="primary"
+        to="/"
+        >Home</v-btn
+      >
+      <!-- <v-btn
           variant="text"
           class="text-uppercase font-weight-bold"
           color="primary"
           >About</v-btn
-        >
-        <v-btn
-          variant="text"
-          class="text-uppercase font-weight-bold"
-          color="primary"
-          to="/contact"
-          >Contact</v-btn
-        >
-        <v-btn
+        > -->
+      <v-btn
+        variant="text"
+        class="text-uppercase font-weight-bold"
+        color="primary"
+        to="/contact"
+        >Contact</v-btn
+      >
+      <!-- <v-btn
           variant="text"
           class="text-uppercase font-weight-bold"
           color="primary"
           >Newsletters</v-btn
-        >
-      </v-row>
+        > -->
     </v-container>
   </v-app-bar>
 </template>
