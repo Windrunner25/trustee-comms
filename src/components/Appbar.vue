87696f30483d6a23ce37e4d33e1e12ba1c962146
Brian Wolf <wolfbj32@gmail.com>
2025-05-11 21:31:11 -0400

    no footer

---
+++ src/components/Appbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <!-- Top Toolbar -->
+  <v-app-bar color="white" flat app height="80">
+    <v-container class="d-flex justify-space-between align-center">
+      <!-- Logo -->
+      <v-row align="center" no-gutters>
+          <v-img
+            src="@/assets/logo-depauw.png"
+            alt="Logo"
+            max-height="100"
+            contain
+            class="mr-4"
+          ></v-img>
+      </v-row>
+
+      <!-- Nav Links -->
+      <v-row class="d-none d-md-flex" align="center" no-gutters>
+        <v-btn
+          variant="text"
+          class="text-uppercase font-weight-bold"
+          color="primary"
+          to="/"
+          >Home</v-btn
+        >
+        <v-btn
+          variant="text"
+          class="text-uppercase font-weight-bold"
+          color="primary"
+          >About</v-btn
+        >
+        <v-btn
+          variant="text"
+          class="text-uppercase font-weight-bold"
+          color="primary"
+          to="/contact"
+          >Contact</v-btn
+        >
+        <v-btn
+          variant="text"
+          class="text-uppercase font-weight-bold"
+          color="primary"
+          >Newsletters</v-btn
+        >
+      </v-row>
+    </v-container>
+  </v-app-bar>
+</template>
